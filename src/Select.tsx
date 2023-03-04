ce6c8644dcf2dfafb88695c99046855ab6693414
kingstockton@gmail.com <>
2023-03-04 16:24:45 -0700

    Add keyboard shorcuts.

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./select.module.css";
 
 export type SelectOption = {
@@ -25,12 +25,48 @@ type SelectProps = {
 export function Select({ multiple, value, onChange, options }: SelectProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [highlightedIndex, setHighlightedIndex] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (isOpen) {
       setHighlightedIndex(1);
     }
   }, [value]);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.target != containerRef.current) {
+        return;
+      }
+      switch (e.code) {
+        case "Enter":
+        case "Space":
+          setIsOpen((prev) => !prev);
+          if (isOpen) selectOption(options[highlightedIndex]);
+          break;
+        case "ArrowUp":
+        case "ArrowDown": {
+          if (!isOpen) {
+            setIsOpen(true);
+            break;
+          }
+          const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1);
+          if (newValue >= 0 && newValue < options.length) {
+            setHighlightedIndex(newValue);
+          }
+          break;
+        }
+        case "Escape":
+          setIsOpen(false);
+          break;
+      }
+    };
+    containerRef.current?.addEventListener("keydown", handler);
+
+    return () => {
+      containerRef.current?.removeEventListener("keydown", handler);
+    };
+  });
 
   function clearOptions() {
     multiple ? onChange([]) : onChange(undefined);
@@ -51,29 +87,34 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
   }
 
   function isOptionSelected(option: SelectOption) {
-    return multiple ? value.includes(option) : option ===  value;
+    return multiple ? value.includes(option) : option === value;
   }
 
   return (
     <div
+      ref={containerRef}
       onBlur={() => setIsOpen(false)}
       onClick={() => setIsOpen((previousState) => !previousState)}
       tabIndex={0}
       className={styles.container}
     >
       <span className={styles.value}>
-        {multiple ? 
-      value.map(v => (
-        <button 
-        key={v.value} 
-        className={styles["option-badge"]}
-        onClick={e => {
-          e.stopPropagation();
-          selectOption(v);
-        }}>{v.label}
-        <span className={styles['remove-btn']}>&times;</span></button>
-      )) : value?.label
-      }</span>
+        {multiple
+          ? value.map((v) => (
+              <button
+                key={v.value}
+                className={styles["option-badge"]}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  selectOption(v);
+                }}
+              >
+                {v.label}
+                <span className={styles["remove-btn"]}>&times;</span>
+              </button>
+            ))
+          : value?.label}
+      </span>
       <button
         onClick={(e) => {
           e.stopPropagation();
